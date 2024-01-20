667b4b8031b82dedb82e22a5e357692aaf93afa8
DiptoChakrabarty <diptochuck123@gmail.com>
2024-01-20 18:19:43 +0530

    right join solution

--- joins/combine_two_tables/solution.sql
+++ joins/combine_two_tables/solution.sql
@@ -1,5 +1,11 @@
-# Write your MySQL query statement below
+/* Solution using Left Join */
 SELECT Person.firstName , Person.lastName, Address.city , Address.state
 FROM Person
 LEFT JOIN Address 
 ON Person.personId=Address.personId;
+
+/* Solution using Right Join */
+SELECT P.firstName , P.lastName, A.city , A.state
+FROM Address A
+RIGHT JOIN Person P
+ON P.personId=A.personId;
