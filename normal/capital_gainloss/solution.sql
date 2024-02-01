577276bc20d2b8386461a3f1b4dac369b57d8b3b
DiptoChakrabarty <diptochuck123@gmail.com>
2024-02-01 21:22:13 +0530

    single condition solution

--- normal/capital_gainloss/solution.sql
+++ normal/capital_gainloss/solution.sql
@@ -2,3 +2,9 @@ SELECT stock_name ,
 SUM(CASE WHEN operation='Sell' THEN price ELSE 0 END) - SUM(CASE WHEN operation='Buy' THEN price END) capital_gain_loss
 FROM Stocks
 GROUP BY stock_name;
+
+/*Less lines but sometimes slower sometimes faster solution*/
+SELECT stock_name , 
+SUM(CASE WHEN operation='Sell' THEN price ELSE -price END) capital_gain_loss
+FROM Stocks
+GROUP BY stock_name; 
