171c1a9b89bc73b46d38cebb696dbf424ca1e76c
DiptoChakrabarty <diptochuck123@gmail.com>
2024-02-27 21:50:47 +0530

    latest time login in 2020

---
+++ normal/lastest_login_2020/solution.sql
@@ -0,0 +1,10 @@
+SELECT user_id, time_stamp as last_stamp
+FROM Logins
+WHERE time_stamp IN
+(
+    SELECT MAX(time_stamp)
+    FROM Logins
+    WHERE YEAR(time_stamp) = '2020'
+    GROUP BY user_id
+    ORDER BY time_stamp
+)
