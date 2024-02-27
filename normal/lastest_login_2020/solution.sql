814d92c54cb071e615f5995130f929ca5c1ee527
DiptoChakrabarty <diptochuck123@gmail.com>
2024-02-27 21:53:19 +0530

    a better smarter solution

--- normal/lastest_login_2020/solution.sql
+++ normal/lastest_login_2020/solution.sql
@@ -1,3 +1,9 @@
+/* Better Solution */
+SELECT user_id, MAX(time_stamp) as last_stamp
+FROM Logins
+WHERE YEAR(time_stamp) = '2020'
+GROUP BY user_id;
+/* My solution*/
 SELECT user_id, time_stamp as last_stamp
 FROM Logins
 WHERE time_stamp IN
